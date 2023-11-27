192166811d8cabbf84f78140d758ba9915c0a449
Hagen Hausser <Hagen.Hausser@Student.HTW-Berlin.de>
2023-11-27 18:39:58 +0100

    initial implementation of frontend. calling backend to fetch all tracks and then display them

---
+++ src/components/RaceButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{
+    track: {"name": string, "country": string, "year": string, "winner": string, "abbreviation": string}
+}>()
+</script>
+
+<template>
+    <div class="race-overview-button">
+        <img class="track-layout" :src="'/track-svgs/' + track.abbreviation + '.svg'" />
+        <div class="race-details">
+            <h3 class="track-name">{{ track.name }}</h3>
+            <div class="country-details">
+                <h4 class="track-country">{{ track.country }}</h4>
+                <img class="country-flag">
+            </div>
+            <div class="latest-details">
+                <span class="latest-year">📅 {{ track.year }}</span>
+                <span class="latest-winner">🏆 {{ track.winner }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .race-overview-button {
+        width: 100%;
+        display: flex;
+        border: white solid 1px;
+        border-radius: 4px;
+        padding: 0.5em;
+        margin: 0.5em;
+        cursor: pointer;
+    }
+
+    .track-layout {
+        width: 4em;
+        height: 6em;
+        margin-right: 1em;
+    }
+
+    .latest-year {
+        margin-right: 0.8em;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import RaceButton from '../components/RaceButton.vue';
+import axios from 'axios';
+</script>
+
+<script lang="ts">
+  export default {
+      data() {
+          return {
+              tracks: [{name: "", country: "", year: "", winner: "", abbreviation: ""}]
+          }
+      },
+      methods: {
+          async getTracks() {
+              try {
+                  const response = await axios.get("http://localhost:8080/api/track/all");
+
+                  this.tracks = response.data;
+              } catch(error) {
+                  console.log(error);
+              }
+          }
+      },
+      created() {
+        this.getTracks();
+      }
+  }
+
+  const races = [{name: "Canada", country: "Canada", year:"2023", winner:"Max Verstappen"},
+  {name: "Bahrain", country: "Bahrain", year:"2023", winner:"Max Verstappen"},
+  {name: "Hungary", country: "Hungary", year:"2023", winner:"Max Verstappen"}]
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <RaceButton v-for="track in tracks" :track="track" />
   </main>
 </template>
